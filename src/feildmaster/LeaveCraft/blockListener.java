197665c5f93936441ca970d318690fff23ce82b2
Feildmaster <admin@feildmaster.com>
2011-08-07 14:08:20 -0500

    v1.1

--- src/feildmaster/LeaveCraft/blockListener.java
+++ src/feildmaster/LeaveCraft/blockListener.java
@@ -7,14 +7,15 @@ import org.bukkit.event.block.LeavesDecayEvent;
 
 class blockListener extends BlockListener {
     public void onBlockPlace(BlockPlaceEvent event) {
-        if(event.getBlock().getType() == Material.LEAVES) {
-            event.getBlock().setData((byte)8); // Player Placed Flag
-        }
+        if(event.getBlock().getType() == Material.LEAVES)
+            event.getBlock().setData((byte)(event.getBlock().getData()+12));
     }
     
     public void onLeavesDecay(LeavesDecayEvent event) {
-        if(!event.isCancelled() && (((int)event.getBlock().getData()) == 8)) { // Player Placed Flag
-            event.setCancelled(true);
-        }
+        if(!event.isCancelled())
+            if(event.getBlock().getData() > 11)
+                event.setCancelled(true);
     }
+    
+    
 }
